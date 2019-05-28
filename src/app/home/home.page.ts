f294735b45b442efe75ba1a158a61ee111cee135
Cindy Siders <css@sypher.com>
2019-05-28 18:57:29 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export interface Department {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  departments: Department[] = [
+    { id: 1, name: 'Department 1' },
+    { id: 2, name: 'Department 2' },
+    { id: 3, name: 'Department 3' },
+    { id: 4, name: 'Department 4' }];
 
+  /** Track By */
+  public trackBy(index: number, value: Department) {
+    return value.id;
+  }
+
+  private deleteDepartment(dept: Department, evt: Event) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    const idx = this.departments.findIndex(m => m.id === dept.id);
+    this.departments.splice(idx, 1);
+  }
 }
